6ec8609382276253ead91cdf2a0dbc1dec21a5c8
Hepsox <marie.delaire@outlook.com>
2024-10-30 13:06:40 +0100

    rename

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
-import { FormProductComponent } from './article/components/form-product/form-product.component';
+import { FormProductComponent } from './product/components/form-product/form-product.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 
